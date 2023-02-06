af2ceb6616c17b3a2655559b1599e733ca3104c7
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2023-02-06 14:38:14 +0200

    modified:   public/style.css 	modified:   src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,17 @@ const HomePage = () => {
     container: {
       height: '420px',
     },
+    title: {
+      with: '100%',
+      textAlign: 'center',
+    },
   };
   return (
     <div style={css.container}>
-      <h1>Home</h1>
+      <h1 style={css.title}>Welcome page for Phone Book</h1>
       <iframe
         title="Dino"
-        src="https://chromedino.com/"
+        src="https://vitalii-volianyk.github.io/goit-react-hw-08-phonebook/dino"
         frameBorder="0"
         scrolling="no"
         width="100%"
